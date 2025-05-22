85dd2227759d0987e5052bbe1ada050b1d8e6fff
tongduychuong <66476208+tongduychuong@users.noreply.github.com>
2025-05-22 09:04:20 +0700

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,10 +11,6 @@
 # fw876/helloworld
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.defaultault
 
-cp -r nss-setup/config-nss.seed .config
-
-sed -i 's/# CONFIG_TARGET_qualcommax_ipq807x_DEVICE_redmi_ax6 is not set/CONFIG_TARGET_qualcommax_ipq807x_DEVICE_redmi_ax6=y/' .config
-
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 
 #sed -i 's@coolsnowwolf/packages@P3TERX/packages@' feeds.conf.default
